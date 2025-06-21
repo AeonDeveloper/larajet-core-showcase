f6e733d7aec5629eb3f00f4eedaf2a73eabc8560
Robin Leah OShell <leah@aeondg.com>
2025-06-21 16:05:41 -0400

    Update SampleGateTest.php

--- examples/SampleGateTest.php
+++ examples/SampleGateTest.php
@@ -1,1 +1,11 @@
+// SAMPLE GATE DEFINITION
 
+Gate::define('manage-visitorinfo', function ($user) {
+    return $user->hasPermission('manage-visitorinfo');
+});
+
+// or dynamically 
+
+foreach (Permission::all() as $permission) {
+    Gate::define($permission->name, fn($user) => $user->hasPermission($permission->name));
+}
